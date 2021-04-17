96b90f2314e748ab78d03c6190791dad05e546f1
egorius <petrenkoeggg@gmail.com>
2021-04-17 15:39:01 +0300

    Added footer to a card + deleted svg + footer now shows all tegs

--- main/models.py
+++ main/models.py
@@ -7,6 +7,7 @@ class Game(models.Model):
     name = models.CharField(unique=True, null=False, max_length=20)
     current_players = models.IntegerField(default=1, null=False)
     created_at = models.DateTimeField(default=timezone.now())
+    tags = models.ManyToManyField('Tag', blank=True, related_name='games')
 
     def __str__(self):
         return self.name
@@ -15,6 +16,10 @@ class Game(models.Model):
         return reverse('game', kwargs={'name': self.name})
 
 
+class Tag(models.Model):
+    title = models.CharField(max_length=30)
 
+    def __str__(self):
+        return ''.join(self.title)
 
 
