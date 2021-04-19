47d458268876fbcf34d14d1e8ea25314c92c78ae
egorius <petrenkoeggg@gmail.com>
2021-04-19 19:52:52 +0300

    Added statiroot

--- TicTac/settings.py
+++ TicTac/settings.py
@@ -121,9 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATIC_ROOT = [
-    os.path.join(BASE_DIR, 'node_modules'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 # Default primary key field type
